4536b021e05753ece53331453e5fc7afaae479c0
CarolinB <carolin.bruederle@gmail.com>
2019-01-03 22:23:36 +0100

    added static folder

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def allblogs(request):
     posts = Blog.objects
     return render(request, 'blog/allblogs.html',
         {'posts': posts})
+
+def detail(request, blog_id):
+    blogDetail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blogDetail})
+    
